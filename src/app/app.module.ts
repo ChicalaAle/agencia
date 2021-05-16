fe4b2ed8dbe9866006c18e202ee279d1f77b51fc
Alejandro Chicala <chicalaale6@gmail.com>
2021-05-15 22:46:46 -0300

    15/5/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WavesComponent } from './extras/waves/waves.component';
+import { BodyComponent } from './components/body/body.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ClientsComponent } from './components/clients/clients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    WavesComponent
+    WavesComponent,
+    BodyComponent,
+    ServicesComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/clients/clients.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper/bundle';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var imageSlider = new Swiper('.image-slider', {
+      autoplay: {
+          delay: 2000,
+          disableOnInteraction: false
+  },
+      loop: true,
+      navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+      spaceBetween: 30,
+      slidesPerView: 5,
+  breakpoints: {
+          // when window is <= 380px
+          380: {
+              slidesPerView: 2,
+              spaceBetween: 10
+          },
+          // when window is <= 516px
+          516: {
+              slidesPerView: 2,
+              spaceBetween: 10
+          },
+          // when window is <= 768px
+          768: {
+              slidesPerView: 3,
+              spaceBetween: 20
+          },
+          // when window is <= 992px
+          992: {
+              slidesPerView: 4,
+              spaceBetween: 30
+          },
+          // when window is <= 1200px
+          1200: {
+              slidesPerView: 5,
+              spaceBetween: 30
+          },
+      }
+  });
+  }
+
+}
