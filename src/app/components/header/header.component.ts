36a57368d324bca624b260515bda3e032af777e4
Alejandro Chicala <chicalaale6@gmail.com>
2021-05-26 07:01:27 -0300

    26/5/2021 - v2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import VanillaTilt from 'vanilla-tilt';
+import anime from 'animejs/lib/anime.es.js';
 
 declare var $:any;
 @Component({
@@ -15,12 +16,26 @@ export class HeaderComponent implements OnInit {
     this.morphext();
 
     let div:HTMLElement = document.querySelector('#til');
+    let div2:HTMLElement = document.querySelector('#prueba');
 
-    VanillaTilt.init(div, {
-      max: 25,
-      speed: 100,
-      reset:false,
-      reverse: true
+    VanillaTilt.init(div2, {
+      max: 15,
+      speed: 5000,
+      reverse: true,
+      // glare: true,
+      // "max-glare": 1
+    });
+
+    anime({
+      targets: '.xd',
+      
+      // translateX: [0, 1000, 300, 0],
+      // translateY: [0, -300, 0],
+      translateX: [0, 1000, 200, 1300, 0],
+      translateY: [0, -700, -100, 0],
+      loop: true,
+      // direction: 'alternate',
+      duration: 15000
     });
 
   }
